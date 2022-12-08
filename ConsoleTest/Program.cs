adc3b2a5f9a3ed260f5f0ca3fc7f76bd9822ca6a
Administrator <Administrator@I-01>
2022-12-08 21:42:22 +0900

    csharptest

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕하세요." + "테스트입니다");
+            Console.WriteLine("유니티 배우는중입니다.");       
+        }
+    }
+}
